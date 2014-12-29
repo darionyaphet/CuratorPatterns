3dc60794f75adc55d966275d5714c99a93433fea
darionyaphet <darion.yaphet@gmail.com>
2014-12-29 23:01:23 +0800

    daily push

--- src/org/darion/yaphet/basic/BasicExamples.java
+++ src/org/darion/yaphet/basic/BasicExamples.java
@@ -4,31 +4,25 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.curator.test.TestingServer;
 
 public class BasicExamples {
 
 	public static void main(String[] args) throws Exception {
-		TestingServer server = new TestingServer();
 
 		// Curator connection instances are allocated from
 		// CuratorFrameworkFactory.
 		// Only need one CuratorFramework object for each ZooKeeper cluster
 		RetryPolicy policy = new ExponentialBackoffRetry(1000, 3);
 		CuratorFramework client = CuratorFrameworkFactory.newClient(
-				server.getConnectString(), policy);
+				"127.0.0.1:2181", policy);
 		client.start();
-
-		if (client.isStarted()) {
-
-		}
-		
 
 		final String path = "/darion/node";
 
 		client.create().forPath(path, "val".getBytes());
+		// client.checkExists().forPath(path);
 		byte[] val = client.getData().forPath(path);
-		System.out.println(new String(val));
+		System.out.println("Path : " + path + " --> " + new String(val));
 		client.delete().forPath(path);
 		client.close();
 	}
---
+++ src/org/darion/yaphet/rpc/Server.java
@@ -0,0 +1,7 @@
+package org.darion.yaphet.rpc;
+
+public class Server {
+	public static void main(String[] args) {
+		curator
+	}
+}

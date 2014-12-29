4f7cff054ad084a2e73a4a7e5d04772bc83299a2
darionyaphet <darion.yaphet@gmail.com>
2014-12-29 23:33:45 +0800

    add thrift support

--- src/org/darion/yaphet/rpc/Server.java
+++ src/org/darion/yaphet/rpc/Server.java
@@ -1,6 +1,9 @@
 package org.darion.yaphet.rpc;
 
+import org.apache.curator.generated.CuratorService;
+
 public class Server {
 	public static void main(String[] args) {
+		// CuratorService.Client
 	}
 }
